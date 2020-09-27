abb409034c9bb26f650cb6171f3d8a866fb06c5f
param26 <sparkros26@gmail.com>
2020-09-27 18:08:20 +0530

    crawler

--- crawl.py
+++ crawl.py
@@ -1,6 +1,7 @@
 # things to note:
 	# make sure you have all the modules
 	# type the correct url(along with http or https)
+	
 import urllib
 import urllib.request, urllib.error, urllib.parse
 from urllib.request import Request, urlopen
@@ -9,23 +10,36 @@ import re
 import requests
 from bs4 import BeautifulSoup as bs
 import os
+
 directory = 'spring'
 if not os.path.exists(directory):
 	os.makedirs(directory)
+	
 url = input("Enter the url: ")
 
+# url = "https://wallhaven.cc/search?tags=car"
 	
 req = Request(url, headers = {'User-Agent':'Mozilla/5.0'})
 html = urllib.request.urlopen(req).read()
 sop = bs(html, 'html.parser')
 
 tags = sop('img')
-# print(tags)
+
+def checkvalidImageUrl(imageUrl):
+	return (imageUrl is None or len(imageUrl) <= 3 or len(re.findall('^.*\.(jpg|JPG|gif|GIF|png|PNG|jpeg|JPEG)$',imageUrl))!=1) 
+	
 counter = 0
 for each_tag in tags:
 	imgs = each_tag.get('src', None)
-	if imgs == None or len(imgs) < 3 or len(re.findall('^.*\.(jpg|JPG|gif|GIF|png|PNG|jpeg|JPEG)$',imgs)) == 0:
-		continue
+	dataimgs = each_tag.get('data-src',None)
+	
+	if checkvalidImageUrl(imgs):
+		if checkvalidImageUrl(dataimgs):
+			continue
+		else:
+			del(imgs)
+			imgs = dataimgs
+			
 	type1 = [] #local addresses
 	type2 = [] 
 	t1 = re.findall('^//', imgs)
@@ -35,25 +49,13 @@ for each_tag in tags:
 		t2 = re.findall('http',imgs)
 		if len(t2) == 0:
 			type2 = 1
-	# print("img ",imgs)
-	# if imgs[0] == '/'
-	# if needFullUrl == 1:
-		# url = url + imgs
-		# req = Request(url, headers = {'User-Agent':'Mozilla/5.0'})
-		# req = url
-		# filename = str(counter) + '.jpg'
-		# urllib.request.URLopener.urlretrieve(req, 'spring/dd.jpg')
-	# else: 
-		# url = imgs
-		# req = Request(url, headers = {'User-Agent':'Mozilla/5.0'})
-		# urllib.request.urlretrieve(url, 'spring/justdid.jpg')
-	# counter+=1
+			
 	opener = urllib.request.build_opener()
 	opener.addheaders=[('User-Agent','Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1941.0 Safari/537.36')]
 	urllib.request.install_opener(opener)
+	
 	if type1 == 1:
 		imgs = "https:"+imgs
-		# print("img made ", imgs)
 	elif type2 == 1:
 		if imgs[0] == '/':
 			imgs = url + imgs
@@ -63,11 +65,13 @@ for each_tag in tags:
 	if len(lastcheck) >= 1:
 		continue;
 	print(imgs)
+	
 	try:
-		urllib.request.urlretrieve(imgs, 'spring/' + str(counter) + '.jpg') #spring is the folder(can rename)
+		urllib.request.urlretrieve(imgs, 'spring/' + str(counter) + '.jpg')
 	except:
 		continue;
 	else:
 		counter+=1;
+		
 print("program was successful")
 
